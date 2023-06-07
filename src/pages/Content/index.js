cb974450b11b6ceeeac1815474a38dae5c8dd651
cheskel <twerskycheskel@gmail.com>
2023-06-08 02:54:13 +0300

    initial commit

---
+++ src/pages/Content/index.js
@@ -0,0 +1,73 @@
+import JSONEditor from 'jsoneditor';
+import 'jsoneditor/dist/jsoneditor.css';
+
+
+function ready() {
+    const plainText = getTextFromTextOnlyDocument();
+    if (!plainText) return
+
+    try {
+        const strippedText = plainText.substring(firstJSONCharIndex(plainText));
+
+        const obj = JSON.parse(strippedText);
+        const container = document.createElement("div")
+        container.style.height = "98vh"
+    
+        const options = {}
+        const editor = new JSONEditor(container, options)
+    
+        editor.set(obj)
+        let pre = document.querySelector('body > pre');
+        if (pre) pre.hidden = true;
+        document.body.appendChild(container);
+
+    } catch {
+        return
+    }
+}
+
+function getTextFromTextOnlyDocument() {
+    const bodyChildren = document.body.childNodes;
+    const firstChild = bodyChildren[0];
+
+    const bodyHasOnlyOneElement = document.body.childNodes.length === 1;
+    const isPre = isPreElement(firstChild);
+    const isPlainText = isPlainTextElement(firstChild);
+
+    if (bodyHasOnlyOneElement && (isPre || isPlainText)) {
+        return firstChild.innerText || firstChild.nodeValue;
+    }
+}
+
+function isPreElement(element) {
+    return element.tagName === 'PRE';
+}
+
+
+function isPlainTextElement(element) {
+    return element.nodeType === Node.TEXT_NODE;
+}
+
+
+
+function firstJSONCharIndex(str) {
+    const arrayIndex = str.indexOf('[');
+    const objIndex = str.indexOf('{');
+    let index = 0;
+  
+    if (arrayIndex !== -1) {
+      index = arrayIndex;
+    }
+  
+    if (objIndex !== -1) {
+      if (arrayIndex === -1) {
+        index = objIndex;
+      } else {
+        index = Math.min(objIndex, arrayIndex);
+      }
+    }
+  
+    return index;
+}
+
+ready()
---
+++ webpack.config.js
@@ -0,0 +1,144 @@
+var webpack = require('webpack'),
+  path = require('path'),
+  fileSystem = require('fs-extra'),
+  env = require('./utils/env'),
+  CopyWebpackPlugin = require('copy-webpack-plugin'),
+  TerserPlugin = require('terser-webpack-plugin');
+var { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+var alias = {
+  'react-dom': '@hot-loader/react-dom',
+};
+
+// load the secrets
+var secretsPath = path.join(__dirname, 'secrets.' + env.NODE_ENV + '.js');
+
+var fileExtensions = [
+  'jpg',
+  'jpeg',
+  'png',
+  'gif',
+  'eot',
+  'otf',
+  'svg',
+  'ttf',
+  'woff',
+  'woff2',
+];
+
+if (fileSystem.existsSync(secretsPath)) {
+  alias['secrets'] = secretsPath;
+}
+
+var options = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.js'),
+  },
+  chromeExtensionBoilerplate: {
+    notHotReload: ['contentScript'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+    publicPath: 'chrome-extension://pifbikkadjogefgbgeddhihjfjboehom/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          }
+        ],
+      },
+      {
+        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+        type: 'asset/resource',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        exclude: /node_modules/,
+      },
+      { test: /\.(ts|tsx)$/, loader: 'ts-loader', exclude: /node_modules/ },
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'source-map-loader',
+          },
+          {
+            loader: 'babel-loader',
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    alias: alias,
+    extensions: fileExtensions
+      .map((extension) => '.' + extension)
+      .concat(['.js', '.jsx', '.ts', '.tsx', '.css']),
+  },
+  plugins: [
+    new CleanWebpackPlugin({ verbose: false }),
+    new webpack.ProgressPlugin(),
+    // expose and write the allowed env vars on the compiled bundle
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/manifest.json',
+          to: path.join(__dirname, 'build'),
+          force: true,
+          transform: function (content, path) {
+            // generates the manifest file using the package.json informations
+            return Buffer.from(
+              JSON.stringify({
+                description: process.env.npm_package_description,
+                version: process.env.npm_package_version,
+                ...JSON.parse(content.toString()),
+              })
+            );
+          },
+        },
+      ],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets/img/icon-128.png',
+          to: path.join(__dirname, 'build'),
+          force: true,
+        },
+      ],
+    })
+  ],
+  infrastructureLogging: {
+    level: 'info',
+  },
+};
+
+if (env.NODE_ENV === 'development') {
+  options.devtool = 'cheap-module-source-map';
+} else {
+  options.optimization = {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  };
+}
+
+module.exports = options;
